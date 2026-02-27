1805c7835fb251b7ca5f1f01f9eb237e2a9f9e80
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2026-02-27 17:07:10 -0500

    refactor: remove parseEnumByIndex and add docs for optional extensions

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -8,19 +8,9 @@ extension SafeRowConvenience on SafeRow {
     return parse<T, String>(key, (dbVal) => values.byName(dbVal));
   }
 
+  /// Parses an optional SQLite String into a Dart Enum using its name.
   T? parseEnumByNameOptional<T extends Enum>(String key, Iterable<T> values) {
     return parseOptional<T, String>(key, (dbVal) => values.byName(dbVal));
-  }
-
-  // --- INT ENUMS ---
-
-  /// Parses a SQLite integer into a Dart Enum using its index declaration.
-  T parseEnumByIndex<T extends Enum>(String key, List<T> values) {
-    return parse<T, int>(key, (index) => values[index]);
-  }
-
-  T? parseEnumByIndexOptional<T extends Enum>(String key, List<T> values) {
-    return parseOptional<T, int>(key, (index) => values[index]);
   }
 
   // --- DATETIME (Example assuming Milliseconds Since Epoch) ---
@@ -30,6 +20,7 @@ extension SafeRowConvenience on SafeRow {
     return parse<DateTime, int>(key, DateTime.fromMillisecondsSinceEpoch);
   }
 
+  /// Parses an optional SQLite integer (epoch) into a Dart DateTime.
   DateTime? parseDateTimeOptional(String key) {
     return parseOptional<DateTime, int>(
         key, DateTime.fromMillisecondsSinceEpoch);
